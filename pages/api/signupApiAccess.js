a8558743a6fb71e174de552ae3a0a15f2eca4eaf
Mahmoud Yazid <abuelyazid.software@gmail.com>
2023-05-08 13:38:59 +0300

    Update signupApiAccess.js

--- pages/api/signupApiAccess.js
+++ pages/api/signupApiAccess.js
@@ -8,8 +8,7 @@ export default  function handle(req,res){
 
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
 
     
     mongoose.connect(ConnectionString).then(()=>{
