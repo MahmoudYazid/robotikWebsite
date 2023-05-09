9ea086997827157182c06f78d9600e28f808ee52
myaz <myaz>
2023-05-09 23:05:56 +0200

    bug of renew request
    bug of renew request

--- pages/api/addrequest.js
+++ pages/api/addrequest.js
@@ -32,25 +32,31 @@ export default  function handler(req,res) {
 
         }).then((answer)=>{
             if(answer.length > 0){
-                
-                 const newreq=new requestssModel({
-            affiliateid:req.query.affiliateid,
-            clientid:req.query.clientid,
-            requestservice:req.query.requestservice,
-            cost:req.query.cost,
-            profit:req.query.profit,
-            requestdate:Date(),
-            answer:'not answered',
-            answercomment:'not answered',
-            AffiliateNationalCard:req.query.affiliatenationalcard,
-            profit:req.query.profit,
-            startdate:'not answered',
-            enddate: req.query.enddate,
+                const today = new Date()
+                const enddate = new Date(answer[0].startdate)
 
-            
-        })
-            newreq.save()
-            return res.json({'results':'ok'})
+
+                enddate.setDate(enddate.getDate() + parseInt(answer[0].enddate))
+
+                if (enddate < today) {
+                    requestssModel.findOneAndUpdate({ _id: `${answer[0]._id}`},{
+                        affiliateid: req.query.affiliateid,
+                        clientid: req.query.clientid,
+                        requestservice: req.query.requestservice,
+                        cost: req.query.cost,
+                        profit: req.query.profit,
+                        requestdate: Date(),
+                        answer: 'not answered',
+                        answercomment: 'not answered',
+                        AffiliateNationalCard: req.query.affiliatenationalcard,
+                        profit: req.query.profit,
+                        startdate: 'not answered',
+                        enddate: req.query.enddate,
+                    })
+                    return res.json({ 'results':'ok' })
+
+                } 
+                 
             }else{
             const newreq=new requestssModel({
             affiliateid:req.query.affiliateid,
