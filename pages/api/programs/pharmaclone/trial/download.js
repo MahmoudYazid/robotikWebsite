f1a1eaea6ee5f7c0b5016daa6a70955b7aabeb8f
Mahmoud Yazid <abuelyazid.software@gmail.com>
2023-05-09 14:08:04 +0300

    Update download.js

--- pages/api/programs/pharmaclone/trial/download.js
+++ pages/api/programs/pharmaclone/trial/download.js
@@ -3,12 +3,11 @@ import path from 'path'
 export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', "*")
     const filepath = path.join(process.cwd(), 'public','pharmaclone_trial.apk')
-    const fileread= fs.readFileSync(filepath)
+    const fileread= fs.createReadStream(filepath)
 
-    res.setHeader('Content-Type', 'application/octet-stream');
+   
     res.setHeader('Content-Disposition', 'attachment; filename=pharmaclone_trial.apk');
-    res.setHeader('Content-Length', fileread.length);
-    res.send(fileread)
+    fileread.pipe(res)
 
 
 }
