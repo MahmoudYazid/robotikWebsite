3ca068a87774472c3081f0a7a053094d54d795d9
Mahmoud Yazid <abuelyazid.software@gmail.com>
2023-05-09 13:54:14 +0300

    Update download.js

--- pages/api/programs/pharmaclone/full/download.js
+++ pages/api/programs/pharmaclone/full/download.js
@@ -5,13 +5,13 @@ export default async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     
     const filePath = path.join(process.cwd(), 'public', 'pharmaclone.apk');
-    const fileContents = fs.readFileSync(filePath);
+    const fileContents = fs.createReadStream(filePath);
 
     res.setHeader('Content-Type', 'application/octet-stream');
     res.setHeader('Content-Disposition', 'attachment; filename=pharmaclone.apk');
     res.setHeader('Content-Length', fileContents.length);
 
-    res.send(fileContents);
+    fileStream.pipe(res);
 
 
 };
