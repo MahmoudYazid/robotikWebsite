9f73119acc8c06f86c757099c8dac16d03a99824
Mahmoud Yazid <abuelyazid.software@gmail.com>
2023-05-09 20:23:32 +0300

    Update addrequest.js
    hekko

--- pages/api/addrequest.js
+++ pages/api/addrequest.js
@@ -32,7 +32,25 @@ export default  function handler(req,res) {
 
         }).then((answer)=>{
             if(answer.length > 0){
-                 return res.json({'results':'n'})
+                
+                 const newreq=new requestssModel({
+            affiliateid:req.query.affiliateid,
+            clientid:req.query.clientid,
+            requestservice:req.query.requestservice,
+            cost:req.query.cost,
+            profit:req.query.profit,
+            requestdate:Date(),
+            answer:'not answered',
+            answercomment:'not answered',
+            AffiliateNationalCard:req.query.affiliatenationalcard,
+            profit:req.query.profit,
+            startdate:'not answered',
+            enddate: req.query.enddate,
+
+            
+        })
+            newreq.save()
+            return res.json({'results':'ok'})
             }else{
             const newreq=new requestssModel({
             affiliateid:req.query.affiliateid,
