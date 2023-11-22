afba222cea0fcefba199f98bc45c4d871181b963
Mahmoud Yazid <abuelyazid.software@gmail.com>
2023-11-22 12:27:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const next = require('next');
-const fs = require('fs');
 const https = require('https');
+const fs = require('fs');
+const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/robotikeg.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/robotikeg.com/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/robotikeg.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/robotikeg.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/robotikeg.com/chain.pem'),
 };
 
 app.prepare().then(() => {
-    const server = express();
+  const server = express();
 
-    server.all('*', (req, res) => {
-        return handle(req, res);
-    });
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
 
-    https.createServer(httpsOptions, server).listen(3000, (err) => {
-        if (err) throw err;
-        console.log('> Ready on https://localhost:3000');
-    });
+  https.createServer(httpsOptions, server).listen(443, (err) => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:443');
+  });
 });
